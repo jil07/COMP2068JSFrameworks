ed897065044e08eef2fbeb95315f5d254590f6fb
jil07 <jilpatelcems@gmail.com>
2023-10-03 20:05:01 -0400

    providing result
    Code to provide the result of performed calculations

--- LAB03/server.js
+++ LAB03/server.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
@@ -17,29 +17,47 @@ const server = http.createServer((req, res) => {
     if (!isNaN(numX) && !isNaN(numY)) {
       let result;
 
-            // Perform the specified math operation
-            switch (method) {
-                case 'add':
-                  result = numX + numY;
-                  break;
-                case 'subtract':
-                  result = numX - numY;
-                  break;
-                case 'multiply':
-                  result = numX * numY;
-                  break;
-                case 'divide':
-                  if (numY === 0) {
-                    res.statusCode = 400; // Bad Request
-                    res.end('Error: Division by zero');
-                    return;
-                  }
-                  result = numX / numY;
-                  break;
-                default:
-                  res.statusCode = 400; // Bad Request
-                  res.end('Error: Invalid method');
-                  return;
-              }
-            }
-        }
+      // Perform the specified math operation
+      switch (method) {
+        case "add":
+          result = numX + numY;
+          break;
+        case "subtract":
+          result = numX - numY;
+          break;
+        case "multiply":
+          result = numX * numY;
+          break;
+        case "divide":
+          if (numY === 0) {
+            res.statusCode = 400; // Bad Request
+            res.end("Error: Division by zero");
+            return;
+          }
+          result = numX / numY;
+          break;
+        default:
+          res.statusCode = 400; // Bad Request
+          res.end("Error: Invalid method");
+          return;
+      }
+
+      // Send the response with the calculated result
+      res.statusCode = 200; // OK
+      res.setHeader("Content-Type", "text/plain");
+      res.end(`${numX} ${method} ${numY} = ${result}`);
+    } else {
+      res.statusCode = 400; // Bad Request
+      res.end("Error: Invalid numbers");
+    }
+  } else {
+    res.statusCode = 400; // Bad Request
+    res.end("Error: Missing parameters");
+  }
+});
+
+// Start the server on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
