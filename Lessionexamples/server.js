cf60194931f2d67a9f7541d27bc0063096e85a74
jil07 <jilpatelcems@gmail.com>
2023-10-14 23:24:23 -0400

    COMPLETED ASSIGNMENT1
    ALL THE CODE HAS BEEN COMPLETED AND INCLUDING THE CSS ANDIMAGES

---
+++ Lessionexamples/server.js
@@ -0,0 +1,14 @@
+//Import Express 
+const express = require("express");
+
+//create app object 
+const app = express();
+
+// Associate Middlewares to paths
+
+
+// make app listen to a port
+app.listen(3000);
+//print a message to confirm everything ran OK
+
+console.log('App running on http:/.localhost:3000');
