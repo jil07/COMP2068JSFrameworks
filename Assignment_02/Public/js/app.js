745cb8ca4589f520b92eb80c760af7da05f97bed
jil07 <jilpatelcems@gmail.com>
2023-12-02 12:06:12 -0500

    adding fxn

--- Assignment_02/Public/js/app.js
+++ Assignment_02/Public/js/app.js
@@ -5,3 +5,16 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Replace these values with  user info
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'user-email@gmail.com',
+    pass: 'user-email-password',
+  },
+});
+
